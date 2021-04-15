2fe16576f106009cb5460ac6b0f001817df62ad2
adham-khatean <aekhatean@gmail.com>
2021-04-16 01:24:13 +0200

    Create RepoCard vue component with is used to show repo name, creation date, number of stars and issues, repo owner's name and avatar. Then added it to the app component inside a loop to represent repos info from Github api.

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,88 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <ul>
+    <li v-for="repo in repos" :key="repo.id">
+      <RepoCard :repoInfo="repo"/>
+    </li>
+  </ul>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { Octokit } from '@octokit/core';
+import RepoCard from './components/RepoCard';
 
 export default {
   name: 'App',
+  created() {
+    this.addRepos();
+  },
+  data() {
+    return {
+      repos: [],
+      page: 1,
+    }
+  },
   components: {
-    HelloWorld
+    RepoCard
+  },
+  methods: {
+    /**
+     * Fetch repos from Github sorted by number of stars.
+     * 
+     * @param {func} callback
+     * @param {int} page
+     */
+    async getRepos( callback, page = 1 ) {
+      const repos = [];
+      const today = new Date().toISOString().slice( 0, 10 );
+      const octokit = new Octokit();
+      const res = await octokit.request( 'GET /search/repositories', {
+        headers: {
+          accept: 'application/vnd.github.v3+json'
+        },
+        q: `created: > ${ today }`,
+        sort: 'stars',
+        order: 'desc',
+        per_page: 100,
+        page: page
+      } );
+
+      repos.push( res.data.items );
+      callback( repos );
+    },
+
+    /**
+     * Callback func to get fetched data from Github by the async func getRepos().
+     */
+    addRepos() {
+      this.getRepos( ( reposList ) => {
+        this.repos = reposList[ 0 ];
+      });
+    }
   }
 }
 </script>
 
 <style>
+body {
+  background: #F1F3F6;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #0e4379;
   margin-top: 60px;
 }
+
+ul {
+  padding: 0;
+}
+
+li {
+  list-style: none;
+  cursor: pointer;
+}
+
 </style>
---
+++ src/components/RepoCard.vue
@@ -0,0 +1,188 @@
+<template>
+    <!-- Only show cards between today and 30 days ago -->
+    <article class="grid-container" v-show="getDateDiff() < 30">
+        <div class="avatar-container">
+            <img :src="repoInfo.owner.avatar_url" alt="avatar">
+        </div>
+        <div class="info-container">
+            <h4 class="repo-title">{{ repoInfo.name }}</h4>
+            <div class="repo-description">
+                <!-- description text should not exceed 100 characters -->
+                <p v-if="repoInfo.description.length < 100">{{ repoInfo.description }}</p>
+                <p v-else>
+                    {{ repoInfo.description.substring( 0, 100 ) }}
+                    <a :href="repoInfo.html_url" target="_blank">..See more</a>
+                </p>
+            </div>
+        </div>
+        <div class="data-container">
+            <small class="repo-stars">{{ repoInfo.stargazers_count }} stars</small>
+            <small class="repo-issues">{{ repoInfo.open_issues }} issues</small>
+            <small class="repo-owner">Created {{ getDateDiff() }} days ago by {{ repoInfo.owner.login }}</small>
+        </div>
+    </article>
+</template>
+
+<script>
+/**
+ * Create card components to show repo name, description, owner name, creation date,
+ * and num of stars and issues.
+ */
+export default {
+    name: 'RepoCard',
+    props: [
+        'repoInfo'
+    ],
+    methods: {
+        /**
+         * Get date diffrence between today and 30 days ago.
+         */
+        getDateDiff() {
+            const today = new Date();
+            const createdDay = new Date( this.repoInfo.created_at);
+            const diffTime = Math.abs( today - createdDay );
+            const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24)); 
+            return diffDays;
+        }
+    }
+}
+</script>
+
+<style scoped>
+a {
+    text-decoration: none;
+}
+.grid-container {
+    display: grid;
+    grid-template-columns: 20% 80%;
+    grid-template-rows: 25% 25% 25% 25%;
+    background-color: #F1F3F6;
+    border-radius: 5px;
+    max-width: 800px;
+    max-height: 200px;
+    margin: 20px auto 20px auto;
+    box-shadow: 6px 6px 6px rgb(55 84 170 / 10%), -3px -3px 11px #ffffff9c;
+    grid-template-areas:
+    "avatar info"
+    "avatar info"
+    "avatar info"
+    "avatar data";
+}
+
+.avatar-container {
+    grid-area: avatar;
+    display: flex;
+}
+
+
+.avatar-container > img {
+    margin: 9px;
+    border-radius: 5px;
+    max-height: 100px;
+    max-width: 100px;
+    align-self: center;
+}
+
+.info-container, .data-container {
+    text-align: left;
+    margin-left: 5px;
+}
+
+.info-container {
+    grid-area: info;
+    display: grid;
+    grid-template-rows: 33.3% 33.3% 33.3%;
+    grid-template-columns: auto;
+    grid-template-areas: 
+    "title"
+    "desc"
+    "desc";
+}
+
+.repo-title {
+    grid-area: title;
+    margin-bottom: 4em;
+    max-width: 500px;
+}
+
+.repo-description {
+    grid-area: desc;
+    max-width: 500px;
+}
+
+
+.data-container {
+    grid-area: data;
+    display: grid;
+    grid-template-columns: 25% 25% 25% 25%;
+    grid-template-rows: auto;
+    grid-template-areas: "stars issues owner owner";
+}
+
+.repo-stars {
+    grid-area: stars;
+}
+
+.repo-issues {
+    grid-area: issues;
+}
+
+.repo-owner {
+    grid-area: owner;
+}
+
+@media only screen and (max-width: 560px){
+    .grid-container {
+        display: grid;
+        grid-template-rows: 25% 25% 25% 25%;
+        grid-template-columns: 33.3% 33.3% 33.3%;
+        grid-template-areas:
+        "avatar info info"
+        "avatar info info"
+        "avatar info info"
+        "data data data";
+        max-height: 300px;
+    }
+
+    .avatar-container {
+        grid-area: avatar;
+        align-self: center;
+    }
+
+    .info-container {
+        grid-area: info;
+        display: grid;
+        grid-template-rows: 33.3% 33.3% 33.3%;
+        grid-template-areas: 
+        "title"
+        "desc"
+        "desc";
+    }
+    .repo-title {
+        grid-area: title;
+        max-width: 245px;
+    }
+    .repo-description {
+        grid-area: desc;
+        max-width: 200px;
+        word-wrap: break-word;
+    }
+
+    .data-container {
+        grid-area: data;
+        display: grid;
+        grid-template-columns: 25% 25% 25% 25%;
+        grid-template-areas: " stars issues owner owner";
+    }
+
+    .repo-stars {
+        grid-area: stars;
+    }
+    .repo-issues {
+        grid-area: issues;
+    }
+    .repo-owner {
+        grid-area: owner;
+    }
+}
+</style>
