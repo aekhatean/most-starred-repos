5533808579ccc1c12048eba9e71222571e65206f
adham-khatean <aekhatean@gmail.com>
2021-04-16 16:48:44 +0200

    Allow most-starred app to go through Github API V3 paginatoin If results exceed the maximum per_page value.

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 # most-starred
+This is a small webapp that lists the most starred github repos that were created in the last 30 days.
+
+## Technologies Used
+
+HTML, CSS, JavaScript, and Vue.js.
+```
+Vue was used in this project because it is samll and suitable for small webapps, but also flexible and scalable with tons of liberaries and its powerful CLI and active community which makes it appealing for bigger scale projects as well.
+
+It is also small in size, so quicker to load and uses less bandwidth.
+```
 
 ## Project setup
 ```
--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,123 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <ul>
+    <li v-for="repo in repos" :key="repo.id">
+      <RepoCard :repoInfo="repo"/>
+    </li>
+  </ul>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { Octokit } from '@octokit/core';
+import RepoCard from './components/RepoCard';
 
 export default {
   name: 'App',
+  created() {
+    this.addRepos();
+  },
+  data() {
+    return {
+      repos: [],
+      pageNum: 0,
+      perPage: 100,
+      remainingReposNum: 0,
+      numReserved: false
+    }
+  },
   components: {
-    HelloWorld
+    RepoCard
+  },
+  methods: {
+    /**
+     * Fetch repos from Github sorted by number of stars.
+     * 
+     * @param {func} callback
+     * @param {int} page
+     */
+    async getRepos( callback, page = 1 ) {
+      const repos = [];
+      const today = new Date().toISOString().slice( 0, 10 );
+      const octokit = new Octokit();
+      const res = await octokit.request( 'GET /search/repositories', {
+        headers: {
+          accept: 'application/vnd.github.v3+json'
+        },
+        q: `created: > ${ today }`,
+        sort: 'stars',
+        order: 'desc',
+        per_page: this.perPage,
+        page: page
+      } );
+
+      // Push the result into the repos array.
+      repos.push( res.data.items );
+
+      // Load more properties if exists (Github API pagination).
+      if( this.remainingReposNum % this.perPage > 0 ) {
+        this.remainingReposNum = this.remainingReposNum - this.perPage;
+        const respons = await this.getRepos( null, this.paginate( this.remainingReposNum, this.perPage, page ) );
+        repos.push( respons );
+      }
+
+      // Do not call the callback func while 
+      if( typeof callback === 'function' ) {
+        callback( repos );
+        this.numReserved = true;
+      }
+    },
+
+    /**
+     * Callback func to get fetched data from Github by the async func getRepos().
+     */
+    addRepos() {
+      this.getRepos( ( reposList ) => {
+        this.repos = reposList[ 0 ];
+        if( !this.numReserved ) {
+          this.remainingReposNum = reposList[ 0 ].length;
+          console.log( this.remainingReposNum );
+        }
+      } );
+    },
+
+    /**
+     * Go through Github API pagination if results exceed the number of results per page (maximum res per page is 100).
+     * 
+     * @param {int} remainingRepos
+     * @param {int} perPage
+     * @param {int} pageNum
+     */
+    paginate( remainingRepos, perPage, pageNum ) {
+      if( remainingRepos % perPage > 0 ) {
+        remainingRepos = remainingRepos - perPage;
+        this.remainingReposNum = remainingRepos;
+        return pageNum++;
+      }
+    }
   }
 }
 </script>
 
 <style>
+body {
+  background: #F1F3F6;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #0e4379;
   margin-top: 60px;
 }
+
+ul {
+  padding: 0;
+}
+
+li {
+  list-style: none;
+  cursor: pointer;
+}
+
 </style>
